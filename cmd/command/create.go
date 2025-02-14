baef91f335bf06d949b1f8b51d6412efc388a2b7
Gungun974 <xfelix974@gmail.com>
2025-02-14 17:25:59 +0400

    Add create command

---
+++ cmd/command/create.go
@@ -0,0 +1,49 @@
+package command
+
+import (
+	"os"
+	"path"
+
+	"github.com/gungun974/gonova/internal/logger"
+	"github.com/gungun974/gonova/internal/utils"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	createCmd.Flags().BoolP("no-git", "", false, "Don't init the project with Git")
+
+	createCmd.Flags().BoolP("postgre", "", false, "Init with postgre module")
+	createCmd.Flags().BoolP("sqlite", "", false, "Init with sqlite module")
+
+	createCmd.Flags().BoolP("nix", "", false, "Init with nix module")
+}
+
+var createCmd = &cobra.Command{
+	Use:   "create [directory-name] [name]",
+	Short: "Create Nova Core in a specific directory",
+	Args:  cobra.MinimumNArgs(2),
+	Run:   CreateNova,
+}
+
+func CreateNova(cmd *cobra.Command, args []string) {
+	err := utils.CreateDirectory(args[0])
+	if err != nil {
+		logger.MainLogger.Fatalf("Failed to create the project directory : %v", err)
+	}
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		logger.MainLogger.Fatalf("Failed to get current directory : %v", err)
+	}
+
+	err = os.Chdir(path.Join(currentDir, args[0]))
+	if err != nil {
+		logger.MainLogger.Fatalf("Failed to switch to the project directory : %v", err)
+	}
+
+	InitNova(cmd, args[1:])
+
+	logger.MainLogger.Info("✨ Nova project has been created.")
+}
