a202f8384ebca5c7039ad91d7a8eb777a3793c2d
Gungun974 <xfelix974@gmail.com>
2025-11-09 15:03:48 +0400

    Add flags on create and init commands for non interactive shell

--- cmd/command/create.go
+++ cmd/command/create.go
@@ -1,16 +1,22 @@
 package command
 
 import (
+	"fmt"
 	"os"
 	"path"
 
 	"github.com/gungun974/gonova/internal/logger"
 	"github.com/gungun974/gonova/internal/utils"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 func init() {
 	rootCmd.AddCommand(createCmd)
+
+	createCmd.Flags().String("database", "none", "Choose the database module: sqlite | postgres | none")
+	createCmd.Flags().Bool("no-git", false, "Skip Git initialization for the project")
+	createCmd.Flags().Bool("nix", false, "Include the Nix module")
 }
 
 var createCmd = &cobra.Command{
@@ -21,6 +27,30 @@ var createCmd = &cobra.Command{
 }
 
 func CreateNova(cmd *cobra.Command, args []string) {
+	isInteractive := term.IsTerminal(int(os.Stdout.Fd()))
+
+	dbFlagSet := cmd.Flags().Changed("database")
+	noGitFlagSet := cmd.Flags().Changed("no-git")
+	nixFlagSet := cmd.Flags().Changed("nix")
+
+	if !isInteractive {
+		if !dbFlagSet || !noGitFlagSet || !nixFlagSet {
+			fmt.Println("Error: In non-interactive mode, all flags must be provided explicitly.")
+			if len(args) == 0 {
+				fmt.Println("Error: In non-interactive mode, all arguments must be provided explicitly.")
+			}
+			fmt.Println()
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+		if len(args) == 0 {
+			fmt.Println("Error: In non-interactive mode, all arguments must be provided explicitly.")
+			fmt.Println()
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+	}
+
 	err := utils.CreateDirectory(args[0])
 	if err != nil {
 		logger.MainLogger.Fatalf("Failed to create the project directory : %v", err)
--- cmd/command/init.go
+++ cmd/command/init.go
@@ -1,14 +1,22 @@
 package command
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gungun974/gonova/internal/actions"
 	"github.com/gungun974/gonova/internal/form"
 	"github.com/gungun974/gonova/internal/logger"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 func init() {
 	rootCmd.AddCommand(initCmd)
+
+	initCmd.Flags().String("database", "none", "Choose the database module: sqlite | postgres | none")
+	initCmd.Flags().Bool("no-git", false, "Skip Git initialization for the project")
+	initCmd.Flags().Bool("nix", false, "Include the Nix module")
 }
 
 var initCmd = &cobra.Command{
@@ -19,6 +27,34 @@ var initCmd = &cobra.Command{
 }
 
 func InitNova(cmd *cobra.Command, args []string) {
+	isInteractive := term.IsTerminal(int(os.Stdout.Fd()))
+
+	databaseFlag, _ := cmd.Flags().GetString("database")
+	noGitFlag, _ := cmd.Flags().GetBool("no-git")
+	nixFlag, _ := cmd.Flags().GetBool("nix")
+
+	dbFlagSet := cmd.Flags().Changed("database")
+	noGitFlagSet := cmd.Flags().Changed("no-git")
+	nixFlagSet := cmd.Flags().Changed("nix")
+
+	if !isInteractive {
+		if !dbFlagSet || !noGitFlagSet || !nixFlagSet {
+			fmt.Println("Error: In non-interactive mode, all flags must be provided explicitly.")
+			if len(args) == 0 {
+				fmt.Println("Error: In non-interactive mode, all arguments must be provided explicitly.")
+			}
+			fmt.Println()
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+		if len(args) == 0 {
+			fmt.Println("Error: In non-interactive mode, all arguments must be provided explicitly.")
+			fmt.Println()
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+	}
+
 	projectName := ""
 	if len(args) == 0 {
 		projectName = form.AskInputWithPlaceholder(
@@ -29,31 +65,55 @@ func InitNova(cmd *cobra.Command, args []string) {
 		projectName = args[0]
 	}
 
-	database := form.AskChoice("Do you want to install a Database module :", []form.Choice[string]{
-		{
-			Name:  "No",
-			Value: "none",
-		},
-		{
-			Name:  "SQLite",
-			Value: "sqlite",
-		},
-		{
-			Name:  "PostgreSQL",
-			Value: "postgre",
-		},
-	})
+	var database string
 
-	enableNoGit := !form.AskOption("Do you want to initialize Git :", true, "Yes", "No")
+	if !dbFlagSet {
+		database = form.AskChoice("Do you want to install a Database module :", []form.Choice[string]{
+			{
+				Name:  "No",
+				Value: "none",
+			},
+			{
+				Name:  "SQLite",
+				Value: "sqlite",
+			},
+			{
+				Name:  "PostgreSQL",
+				Value: "postgres",
+			},
+		})
+	} else {
+		if databaseFlag != "sqlite" && databaseFlag != "postgres" && databaseFlag != "none" {
+			fmt.Printf("Error: invalid value for --database: %q\n", databaseFlag)
+			fmt.Println("Allowed values: sqlite | postgres | none")
+			fmt.Println()
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+		database = databaseFlag
+	}
 
-	enableNix := form.AskOption("Do you want to install the Nix module :", true, "Yes", "No")
+	var enableNoGit bool
+
+	if !noGitFlagSet {
+		enableNoGit = !form.AskOption("Do you want to initialize Git :", true, "Yes", "No")
+	} else {
+		enableNoGit = noGitFlag
+	}
+
+	var enableNix bool
+
+	if !nixFlagSet {
+		enableNix = form.AskOption("Do you want to install the Nix module :", true, "Yes", "No")
+	} else {
+		enableNix = nixFlag
+	}
 
 	if !enableNoGit {
 		err := actions.CheckCanCreateGitRepo()
 		if err != nil {
 			logger.MainLogger.Fatalf("Failed to Check Git : %v", err)
 		}
-
 	}
 
 	err := actions.InstallCore(projectName)
@@ -61,7 +121,7 @@ func InitNova(cmd *cobra.Command, args []string) {
 		logger.MainLogger.Fatalf("Failed to Install Core : %v", err)
 	}
 
-	if database == "postgre" {
+	if database == "postgres" {
 		err := actions.InstallPostgreDatabase()
 		if err != nil {
 			logger.MainLogger.Fatalf("Failed to Install Postgre Database : %v", err)
